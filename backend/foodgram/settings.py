832b24d1fd1c8ffd3cf89342bdd22be16f331a56
Рамиль Мирзазянов <off.nemir.off@mail.ru>
2023-11-20 15:53:58 +0300

    1st try

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -75,7 +75,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB', 'foodgram_db-1'),
         'USER': os.getenv('POSTGRES_USER', 'foodgram_user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'foodgram'),
-        'HOST': os.getenv('DB_HOST', 'db'),
+        'HOST': os.getenv('DB_HOST', 'db-1'),
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
