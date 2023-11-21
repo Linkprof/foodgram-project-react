09c13d1d7540b8747c62d90c2b02324585569c90
Рамиль Мирзазянов <off.nemir.off@mail.ru>
2023-11-21 22:07:02 +0300

    12031023

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,7 @@
+import base64
+
+from django.core.files.base import ContentFile
 from djoser.serializers import UserCreateSerializer, UserSerializer
-from drf_extra_fields.fields import Base64ImageField
 from recipes.models import (Favorite, Ingredient, IngredientsList, Recipes,
                             ShoppingCart, Tag)
 from rest_framework import serializers
@@ -9,13 +11,13 @@ from users.models import Subscribe, User
 MIN_COOK_TIME = 1
 
 
-# class Base64ImageField(serializers.ImageField):
-#     def to_internal_value(self, data):
-#         if isinstance(data, str) and data.startswith('data:image'):
-#             format, imgstr = data.split(';base64,')
-#             ext = format.split('/')[-1]
-#             data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
-#         return super().to_internal_value(data)
+class Base64ImageField(serializers.ImageField):
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith('data:image'):
+            format, imgstr = data.split(';base64,')
+            ext = format.split('/')[-1]
+            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+        return super().to_internal_value(data)
 
 
 class CreateUserSerializer(UserCreateSerializer):
