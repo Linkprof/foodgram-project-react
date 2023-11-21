ee179ff27359626088b24b74ef5471a2cabef15d
Рамиль Мирзазянов <off.nemir.off@mail.ru>
2023-11-21 19:34:42 +0300

    6000

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -13,6 +13,6 @@ router_v1.register('recipes', RecipesViewSet, basename='recipes')
 
 urlpatterns = [
     path('', include(router_v1.urls)),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
+    path('', include('djoser.urls')),
+    path('', include('djoser.urls.authtoken')),
 ]
