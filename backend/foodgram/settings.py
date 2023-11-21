5cf977e158934ba8a8a77d570489cb5cbeb10bd0
Рамиль Мирзазянов <off.nemir.off@mail.ru>
2023-11-21 14:24:20 +0300

    9

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -63,6 +63,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'foodgram.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     'default': {
