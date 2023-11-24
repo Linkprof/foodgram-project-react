a32b87cc6402427976226443f2e651661890e536
Рамиль Мирзазянов <off.nemir.off@mail.ru>
2023-11-24 11:05:05 +0300

    after review a

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -109,10 +109,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/backend_static/'
-STATIC_ROOT = '/app/static/'
+STATIC_ROOT = BASE_DIR / 'backend_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/app/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
